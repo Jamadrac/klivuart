93d63c8a154d14b75eb6e165edb01af64e6e3b41
jamadrac <rabbikindalo001@gmail.com>
2024-07-11 11:16:39 +0200

    asa

---
+++ server/routes/school.js
@@ -0,0 +1,15 @@
+class Addmin{
+// add faculty/ add student /view all studemt/ view all timetables/delete student or faculty 
+}
+
+
+
+class Faculty{
+    // add timetable /add stude results/addttendance/
+}
+
+class student {
+    // student view accademic marks/view timetable
+
+}
+
