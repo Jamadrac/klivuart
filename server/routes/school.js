856a2d9ec94d89da83793533e822ac4d9b6ee0a7
jamadrac <rabbikindalo001@gmail.com>
2024-07-14 22:19:24 +0200

    sd

--- server/routes/school.js
+++ server/routes/school.js
@@ -1,15 +1,27 @@
 class Addmin{
-// add faculty/ add student /view all studemt/ view all timetables/delete student or faculty 
+//   apis for 
+// 1 post: add faculty/ 
+// 2 post: add student 
+// 3 get: view all studemt/ 
+// 4 get: view all timetables
+// 5 get by id delete student or faculty 
 }
 
 
 
-class Faculty{
-    // add timetable /add stude results/addttendance/
+class Faculty{ 
+    // apis for 
+    // 1 add timetable pictude string 
+    // 2 post student results (subjects and makes )
+    // post addttendance( student id, status {pressent or absent, teacher id  }
+     
 }
 
 class student {
-    // student view accademic marks/view timetable
+    //  apis for
+    //  1 student view accademic marks/collect by class id 
+    // 2 view timetable/collect by class id 
+    // 
 
 }
 
